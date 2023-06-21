aa6b80ad941c16379fe51a52384feee39b2b37d3
mina <minaraafatemil14@gmail.com>
2023-06-21 16:19:20 +0300

    MON

--- pall.c
+++ pall.c
@@ -1,19 +1,3 @@
 #include "monty.h"
 
-/**
- * myPall - prints all values on the stack
- * @stack: pointer to top
- * @line_number: line number of the opcode
- */
-void myPall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *current = *stack;
 
-	while (current != NULL)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-		(void)line_number;
-	}
-
-}
--- push.c
+++ push.c
@@ -53,3 +53,20 @@ int is_number(char *str)
 
 	return (1);
 }
+/**
+ * myPall - prints all values on the stack
+ * @stack: pointer to top
+ * @line_number: line number of the opcode
+ */
+void myPall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		(void)line_number;
+	}
+
+}
